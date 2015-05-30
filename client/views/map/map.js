bb4f2af9d786a1acc3ea2cfc8f29e60457f77724
dbeeke <daniel@studiofonkel.nl>
2015-05-30 21:55:28 +0200

    load bug fix?

--- client/views/map/map.js
+++ client/views/map/map.js
@@ -13,48 +13,52 @@ Template.map.rendered = function () {
       hash: true
     })
 
-    var query = Locations.find()
+    map.on('style.load', function() {
+      var query = Locations.find()
 
-    query.observe({
-      added: function (location) {
-        var geojson = EJSON.parse(location.geojson)
+      query.observe({
+        added: function (location) {
 
-        if (geojson) {
+          console.log(location)
 
-          map.addSource(location._id, {
-            "type": "geojson",
-            "data": geojson
-          })
+          var geojson = EJSON.parse(location.geojson)
 
-          map.addLayer({
-            "id": location._id,
-            "type": "symbol",
-            "interactive": true,
-            "source": location._id,
-            "layout": {
-              "line-join": "round",
-              "line-cap": "round",
-              "icon-image": "heart-12",
-              "text-field": location.title,
-              "text-font": "Open Sans Semibold, Arial Unicode MS Bold",
-              "text-offset": [0, 0.6],
-              "text-anchor": "top"
-            },
-            "paint": {
-              "line-color": "#888",
-              "line-width": 8,
-              "text-size": 12,
-            }
-          })
+          if (geojson) {
 
+            map.addSource(location._id, {
+              "type": "geojson",
+              "data": geojson
+            })
+
+            map.addLayer({
+              "id": location._id,
+              "type": "symbol",
+              "interactive": true,
+              "source": location._id,
+              "layout": {
+                "line-join": "round",
+                "line-cap": "round",
+                "icon-image": "heart-12",
+                "text-field": location.title,
+                "text-font": "Open Sans Semibold, Arial Unicode MS Bold",
+                "text-offset": [0, 0.6],
+                "text-anchor": "top"
+              },
+              "paint": {
+                "line-color": "#888",
+                "line-width": 8,
+                "text-size": 12,
+              }
+            })
+          }
         }
-      }
+      })
     })
 
     map.on('click', function(e) {
         map.featuresAt(e.point, {radius: 20}, function(err, features) {
             if (err) throw err
-            if (features[0].layer.id) {
+            if (features[0] && features[0].layer.id) {
               Router.go('/locations/' + features[0].layer.id)
             }
         })
