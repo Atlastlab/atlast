c5440bae725c268534d3340c29200b7c56fa5783
dbeeke <daniel@studiofonkel.nl>
2015-06-03 17:47:36 +0200

    start of tuesday evening

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 AtlastInstance = {
-  mapUrl: 'http://{s}.tilemill.studiofonkel.nl/style/{z}/{x}/{y}.png?id=tmstyle:///home/administrator/styles/geoblog3.tm2&ia2xo3qi'
+  mapUrl: 'http://{s}.tilemill.studiofonkel.nl/style/{z}/{x}/{y}.png?id=tmstyle:///home/administrator/styles/geoblog3.tm2&ia2xo3qi',
+  plugins: {
+    images: {}
+  }
 }
